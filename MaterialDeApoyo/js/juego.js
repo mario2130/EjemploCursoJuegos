d630a0cb937cba6acafcf429c1dc1660cba62e37
Mario Ernesto Villanueva Gutierrez <Mario@MacBook-Pro-de-Mario.local>
2019-06-18 19:25:57 -0400

    test subida

--- MaterialDeApoyo/js/juego.js
+++ MaterialDeApoyo/js/juego.js
@@ -1,3 +1,10 @@
+var nave;
+var balas;
+var timer = 8;
+var delay = 488;
+var fondoJuego;
+
+
 var Iniciar = {
 
     preload: function(){
@@ -5,13 +12,18 @@ juego.load.image('nave', 'imagenes/nave.png');
 juego.load.image('laser', 'imagenes/bala.png');
 juego.load.image('malo', 'imagenes/asteroide.png');
 juego.load.image('bg', 'imagenes/fondo.png');
-    },
+    }, 
     create: function()
     {
-        juego.add.tileSprite(0,0,400,540,'bg');
-        juego.add.tileSprite(0,0,62,59,'nave');
+        fondoJuego = juego.add.tileSprite(0,0,400,540,'bg');
+        //juego.add.tileSprite(0,0,400,540,'bg');
+
+        nave = juego.add.sprite(40,juego.height / 2 ,'nave');
+        nave.anchor.setTo(0.5);
+        juego.physics.startSystem(Phaser.Physics.ARCADE);
+        juego.physics.arcade.enable(nave, true);
     },
     update: function(){
-
+fondoJuego.tilePosition.x -= 3;
     }
 };
