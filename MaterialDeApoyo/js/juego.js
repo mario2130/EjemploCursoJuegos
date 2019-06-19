ff792a71ddbd867f60af5b7a20a81b7b12e88bf5
Giovanni Ibaceta <giovanni.ibaceta@fresenius-kabi.com>
2019-06-18 20:27:18 -0400

    hasta disparar

--- MaterialDeApoyo/js/juego.js
+++ MaterialDeApoyo/js/juego.js
@@ -22,8 +22,45 @@ juego.load.image('bg', 'imagenes/fondo.png');
         nave.anchor.setTo(0.5);
         juego.physics.startSystem(Phaser.Physics.ARCADE);
         juego.physics.arcade.enable(nave, true);
+
+        //limitiar giro de la nave
+        nave.body.allowRotation=false;
+        //crear de balas
+        balas= juego.add.group();
+        balas.enableBody=true;
+        balas.setBodyType= Phaser.Physics.ARCADE;
+        balas.createMultiple(20, 'laser');
+        balas.setAll('anchor.x', 0.5);
+        balas.setAll('anchor.y', 1);
+        balas.setAll('checkWorldBounds', true);
+        balas.setAll('outOfBoundsKill', true);
+
     },
     update: function(){
-fondoJuego.tilePosition.x -= 3;
-    }
+        fondoJuego.tilePosition.x -= 3;
+
+        nave.rotation= juego.physics.arcade.angleToPointer(nave);
+        //disparar Balas
+        
+         if(juego.input.activePointer.isDown){
+            this.disparar();
+         }
+    }, 
+        //disparar una sola bala
+        disparar: function(){
+            
+            timer= juego.time.now + delay;
+            var bala= balas.getFirstDead();
+
+            if(
+                balas.countDead() > 0){
+                  
+                    bala.anchor.setTo(0.5);
+                    bala.reset(nave.x, nave.y);
+                    bala.rotation=
+                    juego.physics.arcade.angleToPointer(bala);
+                    juego.physics.arcade.moveToPointer(bala, 300);
+                }
+                }
+        
 };
